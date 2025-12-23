01b2f1bab0b23558e6c1ee610b1baa8c0e47dcf7
たけちん <97422170+TAKETINGithub@users.noreply.github.com>
2025-12-24 00:59:41 +0900

    Add files via upload

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-12f0f6b9.js',
+                path_to_root + 'searchindex-86aee505.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
